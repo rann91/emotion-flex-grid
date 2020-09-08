19a9a8e962290e6ab05e78d651a02dadb272fea3
Minsik Park <37332261+reallifeliver@users.noreply.github.com>
2020-09-08 12:23:18 +0900

    Update mq.ts
    Object.values does not guarantee the order of array elements. So I think It is needed to be sorted before make it media query array

--- src/app/mq.ts
+++ src/app/mq.ts
@@ -2,7 +2,7 @@ import facepaint from 'facepaint'
 import THEME from './theme'
 
 const mq = (breakpoints: object = THEME.breakpoints) => {
-  const mediaQueries: string[] = Object.values(breakpoints).map(
+  const mediaQueries: string[] = Object.values(breakpoints).sort((a: number, b: number) => a - b ).map(
     (breakpoint: number) => `@media(min-width: ${breakpoint}px)`
   )
 
